b23ee9d6387789fcbd78ba181733af4514454f5d
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 23:02:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
   new_array = (array.take(array.length - 1)).join (", ")
-  other_array = array.pop 
-puts  new_array + "and" + other_array 
+  other_array = array.pop
+puts  new_array + "and" + other_array
 end
